d1a92c6bcc174b025e279901b5cb60f33ad1ea47
ster55 <rm100423@estudante.fieb.edu.br>
2026-04-09 09:11:54 -0300

    JavaScript

--- vetor.js
+++ vetor.js
@@ -1,16 +1,16 @@
 var notas = [7.5, 8.0, 6.0, 9.0];
 
-console.log ("Notas: "+ notas);
-console.log("Primeira nota: " = notas [0]);
-console.log("Segunda nota: " = notas [1]);
-console.log("Terceira nota: " = notas [2]);
-console.log("Quarta nota: " = notas [3]);
+console.log("Notas: " + notas);
+console.log("Primeira nota: " + notas[0]);
+console.log("Segunda nota: " + notas[1]);
+console.log("Terceira nota: " + notas[2]);
+console.log("Quarta nota: " + notas[3]);
 
 //percorrendo o vetor usando um loop
-for (let i = 0 < notas.lentght; ++){
-    console.log("Nota "+(i+1)+ ":" + notas[i])
+for (let i = 0; i < notas.length; i++) {
+    console.log("Nota " + (i + 1) + ": " + notas[i]);
 }
 
 //Usando foreach para percorrer o vetor
-notas.forEach(obj => console.log ("Nota:" + obj))
+notas.forEach(obj => console.log("Nota: " + obj));
 
